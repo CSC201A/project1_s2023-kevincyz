05c8a6ce45433acb583443a15b8d8be4db5350c6
KevinCYZ <cheny220@wfu.edu>
2023-02-01 22:27:06 -0500

    2/1

--- src/Main.java
+++ src/Main.java
@@ -64,6 +64,7 @@ public class Main {
         String shingleString = "";//temporary variable for adding to the array list
         for (int i = 2; i < wordArray.length;++i) {
             shingleString = wordArray[i - 2] + " " + wordArray[i - 1] + " " + wordArray[i];
+            assert(wordArray[i-2]!= "" &&wordArray[i-1]!= ""&&wordArray[i]!= ""):"Empty string encountered";
             Node node = new Node(shingleString);
             boolean repeat = false;
             if (shingles.size() == 0){
